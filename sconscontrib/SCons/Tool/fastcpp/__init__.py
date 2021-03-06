a779ed99b7330ec1656268b6b1edc2607df24159
Mats Wichmann <mats@linux.com>
2021-03-06 06:08:02 -0700

    Run converters on fastcpp and gob2 tools
    Signed-off-by: Mats Wichmann <mats@linux.com>

--- sconscontrib/SCons/Tool/fastcpp/__init__.py
+++ sconscontrib/SCons/Tool/fastcpp/__init__.py
@@ -1,18 +1,6 @@
-
-"""SCons.Tool.fastcpp
-
-A Tool specialized on speeding up large CPP builds with many files.
-It replaces the default list of allowed CPP suffixes and also installs
-a very simple CPP scanner.
-
-There normally shouldn't be any need to import this module directly.
-It will usually be imported through the generic SCons.Tool.Tool()
-selection method.
-
-"""
-
+# MIT License
 #
-# Copyright (c) 2001-7,2010,2011,2012,2013 The SCons Foundation
+# Copyright SCons Foundation
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -32,7 +20,17 @@ selection method.
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
+
+"""SCons.Tool.fastcpp
+
+A Tool specialized on speeding up large CPP builds with many files.
+It replaces the default list of allowed CPP suffixes and also installs
+a very simple CPP scanner.
+
+There normally shouldn't be any need to import this module directly.
+It will usually be imported through the generic SCons.Tool.Tool()
+selection method.
+"""
 
 import os
 import re
@@ -41,6 +39,7 @@ import SCons.Scanner
 import SCons.Environment
 import SCons.Tool
 import SCons.Util
+
 
 def expand_prefixes(env, builder, prefix=None, suffix=None, src_suffix=None):
     if isinstance(builder, SCons.Util.Proxy):
@@ -53,54 +52,55 @@ def expand_prefixes(env, builder, prefix=None, suffix=None, src_suffix=None):
     if src_suffix is not None:
         builder.set_src_suffix(env.subst(src_suffix))
 
+
 def generate(env):
     """Add Builders and construction variables to the Environment."""
 
-    env.SetDefault(
-        FAST_CPPSUFFIXES = ['.c','.cpp','.h']
-    )
+    env.SetDefault(FAST_CPPSUFFIXES=[".c", ".cpp", ".h"])
 
     #
     # Replace default CPP suffix list
     #
-    env['CPPSUFFIXES'] = env['FAST_CPPSUFFIXES']
-    env['OBJPREFIX'] = env.subst('$OBJPREFIX')
-    env['SHOBJPREFIX'] = env.subst('$SHOBJPREFIX')
+    env["CPPSUFFIXES"] = env["FAST_CPPSUFFIXES"]
+    env["OBJPREFIX"] = env.subst("$OBJPREFIX")
+    env["SHOBJPREFIX"] = env.subst("$SHOBJPREFIX")
 
     #
     # Install simple C/CPP scanner
     #
-    cscan = SCons.Scanner.ClassicCPP(name = 'cfile',
-                    suffixes = env['FAST_CPPSUFFIXES'],
-                    path_variable = 'CPPPATH',
-                    regex = r'^\s*(?:include)\s+(<|"?)([^>\s"]+)(?:>|"?)')
-    for suffix in env['FAST_CPPSUFFIXES']:
+    cscan = SCons.Scanner.ClassicCPP(
+        name="cfile",
+        suffixes=env["FAST_CPPSUFFIXES"],
+        path_variable="CPPPATH",
+        regex=r'^\s*(?:include)\s+(<|"?)([^>\s"]+)(?:>|"?)',
+    )
+    for suffix in env["FAST_CPPSUFFIXES"]:
         SCons.Environment._set_SCANNERS(env, suffix, cscan)
 
     #
     # Switching off implicit dependencies for commands like g++ or ar
     #
-    env['IMPLICIT_COMMAND_DEPENDENCIES'] = False
+    env["IMPLICIT_COMMAND_DEPENDENCIES"] = False
 
     #
     # Pre-expanding prefixes/suffixes for the most used builders
     #
     static_obj, shared_obj = SCons.Tool.createObjBuilders(env)
-    expand_prefixes(env, static_obj, '$OBJPREFIX', '$OBJSUFFIX')
-    expand_prefixes(env, shared_obj, '$SHOBJPREFIX', '$SHOBJSUFFIX')
+    expand_prefixes(env, static_obj, "$OBJPREFIX", "$OBJSUFFIX")
+    expand_prefixes(env, shared_obj, "$SHOBJPREFIX", "$SHOBJSUFFIX")
 
     static_lib = SCons.Tool.createStaticLibBuilder(env)
-    expand_prefixes(env, static_lib, '$LIBPREFIX', '$LIBSUFFIX', '$OBJSUFFIX')
+    expand_prefixes(env, static_lib, "$LIBPREFIX", "$LIBSUFFIX", "$OBJSUFFIX")
 
     shared_lib = SCons.Tool.createSharedLibBuilder(env)
-    expand_prefixes(env, shared_lib, '$SHLIBPREFIX', '$SHLIBSUFFIX', '$SHOBJSUFFIX')
+    expand_prefixes(env, shared_lib, "$SHLIBPREFIX", "$SHLIBSUFFIX", "$SHOBJSUFFIX")
 
     module = SCons.Tool.createLoadableModuleBuilder(env)
-    expand_prefixes(env, module, '$LDMODULEPREFIX', '$LDMODULESUFFIX', '$SHOBJSUFFIX')
+    expand_prefixes(env, module, "$LDMODULEPREFIX", "$LDMODULESUFFIX", "$SHOBJSUFFIX")
 
     prog = SCons.Tool.createProgBuilder(env)
-    expand_prefixes(env, prog, '$PROGPREFIX', '$PROGSUFFIX', '$OBJSUFFIX')
+    expand_prefixes(env, prog, "$PROGPREFIX", "$PROGSUFFIX", "$OBJSUFFIX")
+
 
 def exists(env):
     return 1
-
